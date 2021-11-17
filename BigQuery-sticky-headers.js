83a951b4376ab90bfffd8abda34158a0816de231
Ivo <37253375+spijkercenter@users.noreply.github.com>
2021-11-17 16:22:55 +0100

    Insert style tag
    instead of applying to (maybe unloaded) tags

--- BigQuery-sticky-headers.js
+++ BigQuery-sticky-headers.js
@@ -1,18 +1,11 @@
 // ==UserScript==
 // @name         BigQuery sticky headers
-// @namespace    --
-// @version      0.1
-// @description  --
+// @version      0.2
 // @author       Dr Ivol
 // @match        https://console.cloud.google.com/bigquery*
 // @icon         https://symbols.getvecta.com/stencil_75/69_bigquery-icon.63962c35cd.svg
-// @grant        none
+// @licence      unlicense
+// @run-at       document-idle
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    document.getElementsByTagName('th').forEach(th => {
-        th.style.position = 'sticky';
-        th.style.top = 0;
-    });
-})();
+document.head.insertAdjacentHTML("afterbegin", "<style>th{position:-webkit-sticky; position:sticky; top:0;}</style>");
