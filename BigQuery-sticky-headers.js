adc4c831ce5cee899d8acd54061fc56cd8d6d16a
Ivo <37253375+spijkercenter@users.noreply.github.com>
2021-11-23 08:32:22 +0100

    Added description
    Needed according to report https://greasyfork.org/en/reports/19804

--- BigQuery-sticky-headers.js
+++ BigQuery-sticky-headers.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name         BigQuery sticky headers
+// @description  This script will freeze/stick the table headers of the table preview and the query result window in the BigQuery UI. It was tested on Chrome and Firefox and it should work for both the old and the new UI.
 // @version      0.2
 // @author       Dr Ivol
 // @match        https://console.cloud.google.com/bigquery*
